1143559900ed1618a7e84daead55191c614486b4
baygeldi <akylbekuulubaygeldi@gmail.com>
2022-09-30 01:45:02 +0600

    ok

--- 6HOMEWORK/main.swift
+++ 6HOMEWORK/main.swift
@@ -27,10 +27,10 @@ var priceProducts:Float = 0.0
 func addProducts(answer:String){
     if answer == "yes"{
     startToShop(move: move)
-        
+
     }else if answer == "no"{
         print("""
-        
+
         Название продукта            \(namesOfProducts)
 
         Название производителя       \(creatorsOfProducts)
@@ -49,7 +49,7 @@ func addProducts(answer:String){
         print("Хорошо, спасибо за покупку, приходите еще")
     }else{
         print("""
-        
+
         Название продукта            \(namesOfProducts)
 
         Название производителя       \(creatorsOfProducts)
@@ -73,24 +73,24 @@ func addProducts(answer:String){
 
 //Функция которая принтует единичный чек
 func startToShop(move:String){
-    
+
     if move == "yes" {
         func shop(name:String,price1kg:Float,creators:String,weight:Float,discount:Float){
-            
+
             NumberOfShoping += 1
-            
+
             if discount != 0 {
                  priceProducts += (price1kg * weight)
                  discountProduct += (discount * 0.01)
                  priceProducts -= (priceProducts * discountProduct)
-            
-            
-            
-                
+
+
+
+
             }else if discount == 0 {
-                
+
                 priceProducts += (price1kg * weight)
-                
+
             }
             namesOfProducts += ("(\(NumberOfShoping)) \(name),")
             creatorsOfProducts += ("(\(NumberOfShoping)) \(creators),")
@@ -99,17 +99,17 @@ func startToShop(move:String){
             discountOfProducts += ("(\(NumberOfShoping)) \(discount),")
             pricesOfPruducts += ("(\(NumberOfShoping)) \(priceProducts),")
            totalCount += priceProducts
-            
-            
+
+
                 print("""
-        
+
         Название продукта            \(name)
         Название производителя       \(creators)
         Цена за штуку/кг/л/          \(price1kg)
         Количество/вес               \(weight)
         Скидка                       \(discount)
         К оплате                     \(priceProducts)
-        
+
         """)
             }
         print("Напишите название товара (обязательно):")
@@ -129,11 +129,11 @@ func startToShop(move:String){
 
         shop(name: name, price1kg: Float(price1kg) ?? 0, creators: creators,weight:Float(weight) ?? 0,
              discount:Float(discount) ?? 0)
-        
+
         print("Добавить другой товар(yes/no)")
         let answer = readLine()!
             addProducts(answer: answer)
-            
+
     }else if move == "no" {
         print("Ну а зачем ты тогда в магаз пришел?")
     }else{
@@ -145,3 +145,64 @@ let move = readLine()!
 startToShop(move: move)
 
 
+//2 задание с паролем и логином
+
+
+var passwords:[String] = []
+var logins:[String] = []
+var networks:[String] = ["Instagram","Telegram"]
+
+func regInNetworks(log1:String,password1:String,log2:String,password2:String){
+    
+    logins.append(log1)
+    logins.append(log2)
+    
+    passwords.append(password1)
+    
+    if password2 == passwords[0]{
+        
+        print("""
+
+ВНИМАНИЕ
+Такой пароль уже стоит в приложении \(networks[0])
+вы можете ввести его или придумать другой
+
+""")
+       
+        func anotherPassword(password3:String){
+            passwords.append(password3)
+        }
+        
+        print("Придумайте пароль для \(networks[1]):")
+
+        let password3 = readLine()!
+        
+        anotherPassword(password3: password3)
+        
+    } else{
+        
+        passwords.append(password2)
+        
+    }
+}
+
+print("Придумайте логин для \(networks[0]):")
+let log1 = readLine()!
+
+print("Придумайте пароль для \(networks[0]):")
+let password1 = readLine()!
+
+print("Придумайте логин для \(networks[1]):")
+let log2 = readLine()!
+
+print("Придумайте пароль для \(networks[1]):")
+let password2 = readLine()!
+
+regInNetworks(log1: log1, password1: password1, log2: log2, password2: password2)
+
+
+print()
+print("Никому не говорите свои данные!")
+for (index,item) in logins.enumerated(){
+    print("Приложение \(networks[index]): логин - \(item), пароль - \(passwords[index])")
+}
